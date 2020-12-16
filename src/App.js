19dbb3f47f03d0960ba53a5e35705dc587fab388
manuelpaz <manuelpazrobles@gmail.com>
2020-12-15 23:12:04 -0600

    Peticion asincrona.   En este commit se realiza la petición de los datos de forma asincrona   en la continuación de esta practica se mejorara la experiencia de   usuario.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react';
 import logo from './images/logo.png';
-import data from './data.json'
+//import data from './data.json'
 import "./styles.css";
 import './App.css';
 function CharacterCard(props) {
   const { character } = props;
-
   return (
     <div
       className="CharacterCard"
@@ -16,21 +16,42 @@ function CharacterCard(props) {
     </div>
   );
 }
-function App() {
-  return (
-    <div className="container">
-        <div className="App">
-          <img className="Logo" src={logo} alt="Rick y Morty" />
-          <ul className="row">
-            {data.results.map(character =>(
-                <li className="col-6 col-md-3" key={character.id}>
-                       <CharacterCard character={character}/>        
-                </li>  
-            ))}      
-          </ul>
-        </div>
-      </div>
-  );
-}
+class App extends React.Component{
+  //Se inicializa state
+  state = {
+    data: {
+      //Se inicializa results ya que si presenta un error de undefined
+      results: [],
+    },
+  }
+  componentDidMount(){      
+    this.fetchCharacters();      
+  }
+  fetchCharacters = async()=>{
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
 
+    this.setState({
+      data: data,
+
+    })
+
+  }
+  render(){    
+    return (
+      <div className="container">
+          <div className="App">
+            <img className="Logo" src={logo} alt="Rick y Morty" />
+            <ul className="row">
+              {this.state.data.results.map(character =>(
+                  <li className="col-6 col-md-3" key={character.id}>
+                         <CharacterCard character={character}/>        
+                  </li>  
+              ))}      
+            </ul>
+          </div>
+        </div>
+    );
+  }
+  }
 export default App;
