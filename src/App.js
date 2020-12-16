e026eef966090db1d60c63c947320ef9cb964951
manuelpaz <manuelpazrobles@gmail.com>
2020-12-15 22:40:42 -0600

    Presentacion estatica   En este commit se tiene la base para presentar a los personajes sin   embargo los datos se toman de un archivo json local en los siguientes   commits se hara haciendo peticiones de datos asincronas.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import logo from './images/logo.png';
+import data from './data.json'
 import "./styles.css";
 import './App.css';
+function CharacterCard(props) {
+  const { character } = props;
 
+  return (
+    <div
+      className="CharacterCard"
+      style={{ backgroundImage: `url(${character.image})` }}
+    >
+      <div className="CharacterCard__name-container text-truncate">
+        {character.name}
+      </div>
+    </div>
+  );
+}
 function App() {
   return (
     <div className="container">
         <div className="App">
           <img className="Logo" src={logo} alt="Rick y Morty" />
-
-          <ul className="row"> 
-              <li className="col-6 col-md-3" key="1">
-                     Manuel           
-              </li>
-              <li className="col-6 col-md-3" key="2">
-                     Belen           
-              </li>     
+          <ul className="row">
+            {data.results.map(character =>(
+                <li className="col-6 col-md-3" key={character.id}>
+                       <CharacterCard character={character}/>        
+                </li>  
+            ))}      
           </ul>
         </div>
       </div>
