0f8fded5d29adf3255ee9e2e51a5e806d8df014a
manuelpaz <manuelpazrobles@gmail.com>
2020-12-15 22:27:07 -0600

    Primer paso.   Se crea una lista donde se mostrara a los personajes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
+import "./styles.css";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+        <div className="App">
+          <img className="Logo" src={logo} alt="Rick y Morty" />
+
+          <ul className="row"> 
+              <li className="col-6 col-md-3" key="1">
+                     Manuel           
+              </li>
+              <li className="col-6 col-md-3" key="2">
+                     Belen           
+              </li>     
+          </ul>
+        </div>
+      </div>
   );
 }
 
